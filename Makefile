2e1ea67100bab5489c156986d5efeb24519d48c7
ayoubyt <ayoub.aguert@gmail.com>
2021-03-01 23:47:36 +0100

    adding padding function

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ BUILD_DIR := build
 HEADERS_DIR := includes
 
 SRCS := main.c \
+		md5/md5.c
 
 HEADERS = global.h md5.h
 
@@ -17,7 +18,7 @@ LIBFT_DIR := libs/libft
 LIBFT_BIN_DIR := build
 LIBFT_HEADERS_DIR := includes
 LIBFT_SUFF := ft
-	
+
 ################## preprocessing #####################
 
 OBJS := $(addprefix $(BUILD_DIR)/, $(SRCS:.c=.o))
@@ -26,19 +27,16 @@ OBJS_SUBDIRS := $(dir $(OBJS))
 SRCS := $(addprefix $(SRCS_DIR)/, $(SRCS))
 HEADERS := $(addprefix $(HEADERS_DIR)/, $(HEADERS))
 
-$(NAME) := $(BUILD_DIR)/$(NAME)
-
-
 LIBFT_HEADERS_DIR := $(LIBFT_DIR)/$(LIBFT_HEADERS_DIR)
 LIBFT := $(LIBFT_DIR)/$(LIBFT_BIN_DIR)/lib$(LIBFT_SUFF).a
 
 CC := gcc
-CCFLAGS := 
+CCFLAGS := -g
 
 all : $(NAME)
 
 $(NAME): $(OBJS) $(LIBFT)
-	$(CC) $(OBJS) -o $(NAME) $(LIBFT)
+	$(CC) $(CCFLAGS) $(OBJS) $(LIBFT) -o $(NAME)
 
 $(BUILD_DIR)/%.o : $(SRCS_DIR)/%.c $(HEADERS) | $(OBJS_SUBDIRS)
 	gcc -c $< -o $@ $(CCFLAGS) -I $(HEADERS_DIR) -I $(LIBFT_HEADERS_DIR)
@@ -49,4 +47,22 @@ $(LIBFT):
 $(OBJS_SUBDIRS):
 	mkdir -p $@
 
-.PHONY : $(LIBFT)
+clean:
+	rm -rf $(BUILD_DIR)
+	make clean -C $(LIBFT_DIR)
+
+fclean: clean
+	rm -rf $(NAME)
+	make fclean -C $(LIBFT_DIR)
+
+
+re: fclean all
+
+re-all: re
+	make re -C $(LIBFT_BIN_DIR)
+
+
+# .PHONY : $(LIBFT)
+
+.PHONY:
+	all clean fclean
--- srcs/md5/md5.c
+++ srcs/md5/md5.c
@@ -1,33 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   md5.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayoubyt <ayoubyt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/01 23:32:16 by ayoubyt           #+#    #+#             */
+/*   Updated: 2021/03/01 23:40:01 by ayoubyt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "md5.h"
 
-/*
-** message must padded to satisfy the folowing conditions
-** 
-*/
-
-char*		ft_get_padded_data(char* data,
-								size_t len,
-								char* padded_data,
-								size_t* new_len)
+char		*ft_get_padded_data(
+							char *data,
+							size_t len,
+							char **result_data,
+							size_t *result_len
+)
 {
 	int			new_len;
-	char*		padded_data;
-	int			padded_len;
+	char		*padded_data;
+	int			added_len;
 	uint64_t	original_length_int_bits;
 
-	padded_len = (len % 64 ? 64 : (64 - len % 64));
-	new_len = len + padded_len;
+	added_len = (len % 64 ? (64 - len % 64) : 64);
+	new_len = len + added_len;
+	padded_data = malloc(new_len);
 	ft_memcpy(padded_data, data, len);
 	padded_data[len] = 1;
-	ft_bzero(padded_data + len + 1, padded_len - 1);
+	ft_bzero(padded_data + len + 1, added_len - 1);
 	original_length_int_bits = (len * 8);
-	ft_memcpy(data + (new_len - 8), &original_length_int_bits, 8);
+	ft_memcpy(padded_data + (new_len - 8), &original_length_int_bits, 8);
+	*result_data = padded_data;
+	*result_len = new_len;
 }
 
-uint32_t ft_md5(char *data, size_t len)
+uint32_t	ft_md5(char *data, size_t len)
 {
-	int		new_len;
-	char*	padded_data;
+	size_t		new_len;
+	char		*padded_data;
 
-
+	ft_get_padded_data(data, len, &padded_data, &new_len);
 }
