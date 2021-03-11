436242afa3ab87d77830454f5717b632544e3ce5
ayoubyt <ayoub.aguert@gmail.com>
2021-03-11 12:34:50 +0100

    worging on cmd args parcing

---
+++ includes/ft_arg_parse.h
@@ -0,0 +1,28 @@
+#ifndef FT_ARG_PARSE
+#define FT_ARG_PARSE
+
+#include "libft.h"
+
+typedef unsigned char bool_t;
+
+typedef enum
+{
+	MD5,
+	SHA256
+} command_t;
+
+/*
+
+*/
+
+typedef struct
+{
+	command_t	cmd;
+	bool_t	 	s_opt;
+	char 		*s_opt_param;
+	bool_t 		q_opt;
+	bool_t 		p_opt;
+	bool_t 		r_opt;
+} args_t;
+
+#endif
